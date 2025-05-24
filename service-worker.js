1644daa320b3b216d0a5e42e75df5b7705cd36f0
nibitanga-aime <nibitangaaime597@gmail.com>
2025-05-24 14:02:56 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const cacheName = "battery-app-v1";
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./favicon.png",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
